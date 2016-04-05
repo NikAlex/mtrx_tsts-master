cad8d4ced14a7d1c5785ca93b0e74cf515c4fc0d
NikAlex <alez90@yandex.ru>
2016-04-05 20:06:27 +0300

    mm

---
+++ include/matrix.h
@@ -0,0 +1,49 @@
+#ifndef Matrix_h
+#define Matrix_h
+
+
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+class matrix
+{
+public:
+	matrix();
+	matrix(int _cols, int _rows);
+	matrix(const matrix &Matrix);
+	matrix(int _rows, int _columns, int time);
+	~matrix();
+	void get_line(int k);
+	void get_from_file(string _name);
+	void print_matrix() const;
+	void copy_matrix(const matrix &Matrix);
+	matrix &operator=(const matrix &Matrix);
+	friend matrix operator+(const matrix &matrix_1, const matrix &matrix_2);
+	friend matrix operator*(const matrix &matrix_1, const matrix &matrix_2);
+	int *operator[] (int i) const;
+	void get_sum_r(const matrix &matrix_1, const matrix &matrix_2, int s);
+	void get_multi_r(const matrix &matrix_1, const matrix &matrix_2, int m);
+	void create_memory();
+	int get_num_rows() const;
+	int get_num_cols() const;
+	int get(int rows, int columns) const;
+	void set(int row, int columns, int set);
+	bool operator== (const matrix &right) const; 
+private:
+	int rows;
+	int columns;
+	int **_matrix;
+};
+
+#endif
+
+
---
+++ tests/source/init.cpp
@@ -0,0 +1,50 @@
+#include <matrix.h>
+#include <catch.hpp>
+#include <fstream>
+
+SCENARIO("Matrix init", "[init]") {
+
+	GIVEN("The number of rows and columns") {
+
+		auto rows = 3;
+		auto columns = 3;
+
+		WHEN("Create instansce of Matrix") {
+			matrix matrix(rows, columns);
+			THEN("The number of rows and columns must be preserved") {
+
+				REQUIRE(matrix.get_num_rows() == rows);
+				REQUIRE(matrix.get_num_cols() == columns);
+			}
+		}
+	}
+}
+SCENARIO("matrix +", "[addition]") {
+	matrix A = matrix(2, 2);
+	A.get_from_file("A2x2.txt");
+	matrix B = matrix(2, 2);
+	B.get_from_file("B2x2.txt");
+	matrix expected = matrix(2, 2);
+	expected.get_from_file("A+B2x2.txt");
+	matrix result = A + B;
+	REQUIRE(result == expected);
+}
+
+SCENARIO("matrix *", "[addition]") {
+	matrix A = matrix(2, 2);
+	A.get_from_file("A2x2.txt");
+	matrix B = matrix(2, 2);
+	B.get_from_file("B2x2.txt");
+	matrix expected = matrix(2, 2);
+	expected.get_from_file("A*B2x2.txt");
+	matrix result = A * B;
+	REQUIRE(result == expected);
+}
+
+SCENARIO("matrix: operator ==", "[equal]") {
+	matrix A, B;
+	A.get_from_file("A2x2.txt");
+	B.get_from_file("A2x2.txt");
+	bool f = (A == B);
+	REQUIRE(f);
+}
